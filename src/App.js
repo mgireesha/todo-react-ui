4f312dce6e6ad816cf73cca94d861b478f925ca1
mgireesha <mgireesha77@gmail.com>
2022-02-14 00:02:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useEffect } from 'react';
+import { React } from 'react';
 import './App.css';
 import { Header } from './components/Header.js';
 import { Body } from './components/Body.js';
